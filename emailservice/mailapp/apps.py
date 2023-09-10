cc4f456f59d8a091cd33d42ea62238f942b10775
divinejoshua <divineuche399@gmail.com>
2023-09-10 13:04:06 +0100

    Added mailapp

--- emailservice/mailapp/apps.py
+++ emailservice/mailapp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class EmailappConfig(AppConfig):
+class MailappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'emailapp'
+    name = 'mailapp'
