3fdb6690541d11891d12551a27cf5e7465165bb7
divinejoshua <divineuche399@gmail.com>
2023-09-10 12:28:58 +0100

    Added urls.py

--- userservice/accounts/views.py
+++ userservice/accounts/views.py
@@ -12,11 +12,18 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     #permission_classes = (AllowAny,)
     serializer_class = RegisterSerializer
+
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
+
+        # Save user to database 
+        # self.perform_create(serializer)
+
+        # Publish message to Kafka producer
         producerUserCreated.publish("user_created_method",json.dumps(serializer.data))
+
+
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
       
